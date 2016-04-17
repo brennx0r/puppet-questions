547a778ba045ca7ba10f14c70434e5a1f9eb4681
brennx0r <brennx0r@gmail.com>
2016-04-17 12:22:43 -0700

    Initial commit of 100 new unique users script
    Focus on the initial commit is the creation of the file, basic inputs, and the creation of the ID within the file.

---
+++ scripting/create-100-unique-users.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Let i have input from use
+i=$1
+FILENAME=$2
+
+#Handle condition where there's no or 'incorrect' input from the user
+
+		# Condition where no input from user
+
+		# Condition where integer value is not a positive integer value
+
+
+#Handle condition where there's no input on the filename or filename is not valid
+
+		# Condition where no input from user
+
+		# Condition where no input from user
+
+
+
+
+# Create the lines for the output file
+for i in {1..100}; do
+  echo $i >> $2
+done 
